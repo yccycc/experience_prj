e8445422b61016dc9c4045e02db941c04c69cd0c
Administrator <772878320@qq.com>
2016-03-31 08:15:05 +0800

    start pop window

---
+++ precious/src/com/yctech/myapp/test/ToDealGdManager.java
@@ -0,0 +1,13 @@
+package com.yctech.myapp.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.yctech.myapp.R;
+
+public class ToDealGdManager extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_deal_gd_manager);
+    }
+}
